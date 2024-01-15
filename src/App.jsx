05f9e948dd6b39f7c7622eddb627f95d87cf969b
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-01-15 17:24:38 -0600

    added router and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,61 @@
-import styled from "styled-components";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+import Users from "./pages/Users.jsx";
+import Settings from "./pages/Settings.jsx";
+import Account from "./pages/Account.jsx";
+import Cabins from "./pages/Cabins.jsx";
+import Bookings from "./pages/Bookings.jsx";
+import Login from "./pages/Login.jsx";
 import GlobalStyles from "./styles/GlobalStyles.js";
-import Button from "./ui/Button.jsx";
-import Input from "./ui/Input.jsx";
 
-const H1 = styled.h1`
-    font-size: 30px;
-    font-weight: 600;
-    background-color: yellow;
-`
 
-const StyledApp = styled.div`
-    background-color: orangered;
-    padding: 20px;
-`
+// const StyledApp = styled.div`
+//     background-color: white;
+//     padding: 20px;
+// `
+
 function App() {
 
     return (
         <>
         <GlobalStyles/>
-        <StyledApp>
-            <H1>The Wild Oasis!</H1>
-            <Button>Check In</Button>
-            <Button>Check Out</Button>
-
-            <Input type={'number'} placeholder={'Number of Guests'}/>
-        </StyledApp>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Navigate replace to={'dashboard'}/>}/>
+                <Route path={'dashboard'} element={<Dashboard/>}/>
+                <Route path={'bookings'} element={<Bookings/>}/>
+                <Route path={'cabins'} element={<Cabins/>}/>
+                <Route path={'users'} element={<Users/>}/>
+                <Route path={'settings'} element={<Settings/>}/>
+                <Route path={'account'} element={<Account/>}/>
+                <Route path={'login'} element={<Login/>}/>
+                <Route path={'*'} element={<PageNotFound/>}/>
+            </Routes>
+        </BrowserRouter>
         </>
+
+
+        // <>
+        //     <GlobalStyles/>
+        //     <StyledApp>
+        //         <Row type={'horizontal'}>
+        //                 <Heading type={'h1'}>The Wild Oasis!</Heading>
+        //             <div>
+        //                 <Heading type={'h2'}>Check In and Out</Heading>
+        //                 <Button variation={'primary'} size={'medium'}>Check In</Button>
+        //                 <Button variation={'secondary'} size={'small'}>Check Out</Button>
+        //             </div>
+        //         </Row>
+        //         <Row type={'vertical'}>
+        //             <Heading as={'h3'}>Forms</Heading>
+        //             <form>
+        //             <Input type={'number'} placeholder={'Number of Guests'}/>
+        //             <Input type={'number'} placeholder={'Number of Guests'}/>
+        //             </form>
+        //         </Row>
+        //     </StyledApp>
+        // </>
     )
 }
 
