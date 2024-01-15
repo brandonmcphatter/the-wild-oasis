629b3c6cf95bcdf632049b10a9e6a4d880cfba47
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-01-15 14:17:23 -0600

    creating styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles.js";
+import Button from "./ui/Button.jsx";
+import Input from "./ui/Input.jsx";
 
 const H1 = styled.h1`
     font-size: 30px;
     font-weight: 600;
     background-color: yellow;
-`
-
-const Button = styled.button`
-    font-size: 1.4rem;
-    padding: 1.2rem 1.6rem;
-    font-weight: 500;
-    border: none;
-    border-radius: 7px;
-    background-color: purple;
-    color: white;
-    margin: 20px;
-    cursor: pointer;
-`
-
-const Input = styled.input`
-border: 1px solid #ddd;
-    border-radius: 5px;
-    padding: 0.8rem 1.2rem;
 `
 
 const StyledApp = styled.div`
@@ -31,6 +16,8 @@ const StyledApp = styled.div`
 function App() {
 
     return (
+        <>
+        <GlobalStyles/>
         <StyledApp>
             <H1>The Wild Oasis!</H1>
             <Button>Check In</Button>
@@ -38,6 +25,7 @@ function App() {
 
             <Input type={'number'} placeholder={'Number of Guests'}/>
         </StyledApp>
+        </>
     )
 }
 
