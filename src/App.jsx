ff6a5496fd449ee9298019c8c5e1d662b7589788
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-01-15 18:22:25 -0600

    built and styled layout with functional sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Cabins from "./pages/Cabins.jsx";
 import Bookings from "./pages/Bookings.jsx";
 import Login from "./pages/Login.jsx";
 import GlobalStyles from "./styles/GlobalStyles.js";
+import AppLayout from "./ui/AppLayout.jsx";
 
 
 // const StyledApp = styled.div`
@@ -19,20 +20,22 @@ function App() {
 
     return (
         <>
-        <GlobalStyles/>
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Navigate replace to={'dashboard'}/>}/>
-                <Route path={'dashboard'} element={<Dashboard/>}/>
-                <Route path={'bookings'} element={<Bookings/>}/>
-                <Route path={'cabins'} element={<Cabins/>}/>
-                <Route path={'users'} element={<Users/>}/>
-                <Route path={'settings'} element={<Settings/>}/>
-                <Route path={'account'} element={<Account/>}/>
-                <Route path={'login'} element={<Login/>}/>
-                <Route path={'*'} element={<PageNotFound/>}/>
-            </Routes>
-        </BrowserRouter>
+            <GlobalStyles/>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<AppLayout/>}>
+                        <Route index element={<Navigate replace to={'dashboard'}/>}/>
+                        <Route path={'dashboard'} element={<Dashboard/>}/>
+                        <Route path={'bookings'} element={<Bookings/>}/>
+                        <Route path={'cabins'} element={<Cabins/>}/>
+                        <Route path={'users'} element={<Users/>}/>
+                        <Route path={'settings'} element={<Settings/>}/>
+                        <Route path={'account'} element={<Account/>}/>
+                    </Route>
+                        <Route path={'login'} element={<Login/>}/>
+                        <Route path={'*'} element={<PageNotFound/>}/>
+                </Routes>
+            </BrowserRouter>
         </>
 
 
